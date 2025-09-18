8e0e70724c50a680b10c62cddda9767fba0188de
JerryRivera609 <jerry609rivera@gmail.com>
2025-09-18 18:02:31 -0500

    Cambios en operaciones

--- src/pages/Operaciones/ControlVencimiento.jsx
+++ src/pages/Operaciones/ControlVencimiento.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 export default function ControlVencimiento() {
     
   return (
-    <h1>CONTROL DE VENCIMIENTOS</h1>
+    <h1>OPERACIONES CONTROL DE VENCIMIENTOS</h1>
+    
   );
 }
