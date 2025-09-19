bea777ac1f6842bafccf6e3ca862d80c3f6ef9f0
Crishub280 <catrrobert@gmail.com>
2025-09-19 17:51:45 -0500

    prueba merge 4

--- src/pages/Ventas/components/pasos/PasoVehiculo.jsx
+++ src/pages/Ventas/components/pasos/PasoVehiculo.jsx
@@ -2,13 +2,11 @@ import InputField from "../../../../components/InputField";
 import SelectField from "../../../../components/SelectField";
 import SearchSection from "../../../../components/SearchSection";
 import FormGrid from "../../../../components/FormGrid";
-
 import { useEffect, useState } from "react";
 import { FaSearch } from "react-icons/fa";
-
 import { buscarVehiculoPorPlaca } from "../../../../api/vehiculos";
-import { } from "../../../../api/mapfreservice";
-import { listarMarcasPositiva, listarModelosPositiva, listarVerionesPositiva } from "../../../../api/positivaservice";
+import {} from "../../../../api/mapfreservice";
+import { listarMarcasPositiva,listarModelosPositiva,listarVerionesPositiva } from "../../../../api/positivaservice";
 import ubigeo from "../../../../data/ubigeo/ubigeo_optimizado.json";
 import { tiposYUsosCompletos, placas } from "../../../../data/usos";
 
@@ -26,6 +24,14 @@ export default function PasoVehiculo({ data, onChange }) {
         <thead>
           <tr className="bg-gray-100">
             <th className="border px-4 py-2">Columna 1</th>
+            <th className="border px-4 py-2">Columna 2</th>
+            <th className="border px-4 py-2">Columna 3</th>
+            <th className="border px-4 py-2">Columna 3</th>
+            <th className="border px-4 py-2">Columna 3</th>
+            <th className="border px-4 py-2">Columna 3</th>
+            <th className="border px-4 py-2">Columna 3</th>
+            <th className="border px-4 py-2">Columna 3</th>
+            <th className="border px-4 py-2">Columna 3</th>
           </tr>
         </thead>
         <tbody>
