c9243c97d33c681342d3e361639f6eedf75e6bc6
JerryRivera609 <jerry609rivera@gmail.com>
2025-10-24 15:51:44 -0500

    Jerry - Generar excel de polizas filtradas

--- src/pages/Operaciones/ControlVencimiento.jsx
+++ src/pages/Operaciones/ControlVencimiento.jsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 export default function ControlVencimiento() {
     
   return (
-    <h1>OPERACIONES CONTROLD DE VENCIMIENTOS</h1>
+    <h1>OPERACIONES CONTROL DE VENCIMIENTOS</h1>
     
   );
 }
